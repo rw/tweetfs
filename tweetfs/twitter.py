a2abf6315eec4c536e4384f4ebf5c070500bca9a
rw <robert.winslow@gmail.com>
2011-09-03 20:58:36 -0400

    seqtweet integration

---
+++ tweetfs/twitter.py
@@ -0,0 +1,15 @@
+from seqtweet.seqtweet import SeqTweet
+
+def read_creds():
+    from creds import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+    return [CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET]
+
+def make_client():
+    client = SeqTweet(*read_creds())
+    return client
+
+def make_uploader(client):
+    return lambda s: client.create(s)
+
+def make_downloader(client):
+    return lambda tweet_id: client.read(tweet_id)
