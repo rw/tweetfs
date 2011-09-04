1d08bfcbe3d72ca99258db6cb65fa97a263ce326
rw <robert.winslow@gmail.com>
2011-09-03 21:00:00 -0400

    add assert_type(...) for validating types

--- tweetfs/util.py
+++ tweetfs/util.py
@@ -3,3 +3,8 @@ def is_file(x):
 
 def is_dir(x):
     return x['type'] == 'dir'
+
+def assert_type(x, expected_type, prefix='generic error'):
+    if type(x) != expected_type:
+        raise RuntimeError('%s: expected %s, got %s' % \
+                           (prefix, expected_type, type(x)))
