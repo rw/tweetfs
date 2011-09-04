028135c2fb68a312c492a4d546a4de2546655852
rw <robert.winslow@gmail.com>
2011-09-03 20:59:30 -0400

    concealer (plainsight) and unicode support for pack module

--- tweetfs/pack.py
+++ tweetfs/pack.py
@@ -1,4 +1,5 @@
 # pack a file or directory into a dict
+from bitstring import BitArray
 import bson
 from os.path import exists, split
 import os
@@ -9,8 +10,8 @@ def fatal_if_nexists(name, kind):
        raise RuntimeError('FATAL: %s "%s" does not exist' % (kind, name))
 
 def serialize(x):
-    s = bson.dumps(x)
-    if not type(s) is str:
+    s = unicode(bson.dumps(x))
+    if not type(s) is unicode:
         raise ArgumentError('FATAL: bad string "%s"' % s)
     return s
 
@@ -29,13 +30,14 @@ def files(dir):
     return [name for name in os.listdir(dir)
             if os.path.isfile(os.path.join(dir, name))]
 
-def pack(node, uploader):
+def pack(node, uploader, concealer):
     if os.path.isfile(node):
         print 'file: %s' % node
-        payload = serialize({'type': 'file',
-                             'name': node,
-                             'data': read_file(node)})
-        external_id = uploader(payload)
+        serialized_payload = BitArray(bytes=serialize({'type': 'file',
+                                                       'name': node,
+                                                       'data': read_file(node)}))
+        hidden_payload = concealer.conceal(serialized_payload)
+        external_id = uploader(hidden_payload)
         return external_id
     else:
         print 'dir: %s: %s' % (node, os.listdir(node))
@@ -44,8 +46,9 @@ def pack(node, uploader):
         for child in os.listdir('.'):
             external_ids.append(pack(child, uploader))
         os.chdir('..')
-        payload = serialize({'type': 'dir',
-                             'name': node,
-                             'ids': external_ids})
-        external_id = uploader(payload)
+        serialized_payload = BitArray(bytes=serialize({'type': 'dir',
+                                                       'name': node,
+                                                       'ids': external_ids}))
+        hidden_payload = concealer.conceal(serialized_payload)
+        external_id = uploader(hidden_payload)
         return external_id
